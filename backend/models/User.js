978e35939d7382a8cd7d156d2db2e5711ee16446
yashraj21fr <yashraj21c@gmail.com>
2024-09-13 22:21:51 +0530

    Add files via upload

---
+++ backend/models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+// User Schema
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ["student", "admin"], default: "student" },
+});
+
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
