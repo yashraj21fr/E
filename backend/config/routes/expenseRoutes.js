50f400ce23437fd7111531b252a3417636d5adb9
yashraj21fr <yashraj21c@gmail.com>
2024-09-13 22:30:51 +0530

    Delete backend/config directory

--- backend/config/routes/expenseRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { addExpense, getExpenses } = require("../controllers/expenseController");
-const router = express.Router();
-
-router.post("/add", addExpense);
-router.get("/view", getExpenses);
-
-module.exports = router;
